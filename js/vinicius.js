5c315ff8a14720c4f0e2a45ab4dbc2f6c62bd347
Vinicius Victor <vine.victordev@gmail.com>
2023-02-27 18:24:53 -0300

    Implementaçâo Pagina Vinicius, Local Storage e Edição por JS dos Markers

--- js/vinicius.js
+++ js/vinicius.js
@@ -21,5 +21,21 @@ if (window.localStorage.getItem("textoSalvoLocal") != undefined) {
 }
 
 function ValorLocalStorage() {
-  span.innerHTML = window.localStorage.getItem("textoSalvoLocal");
+  divTexto.innerHTML = window.localStorage.getItem("textoSalvoLocal");
 }
+
+let marker1 = document.getElementById("marker1");
+var marker2 = document.getElementById("marker2");
+var marker3 = document.getElementById("marker3");
+var marker4 = document.getElementById("marker4");
+var marker5 = document.getElementById("marker5");
+var marker6 = document.getElementById("marker6");
+var marker7 = document.getElementById("marker7");
+
+marker1.style.left = "30vw";
+marker2.style.left = "10vw";
+marker3.style.left = "14vw";
+marker4.style.left = "2vw";
+marker5.style.left = "18vw";
+marker6.style.left = "18vw";
+marker7.style.left = "13vw";
